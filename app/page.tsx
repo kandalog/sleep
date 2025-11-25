83aa1a24a003f4cb1ff4af11e7fd6d1880598649
kandalog <asuasu.tee@gmail.com>
2025-11-26 02:33:35 +0900

    feat: 睡眠記録UIの雛形

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,77 @@
 export default function Home() {
   return (
-    <>
-      <p>home</p>
-    </>
+    <div className="max-w-[1200px] mx-auto">
+      {/* ヘッダー */}
+      <header className="py-3 border-b border-gray-300">
+        <h1 className="font-bold text-2xl text-center">睡眠記録</h1>
+      </header>
+
+      {/* 期間 */}
+      <div className="flex justify-between items-center py-4 px-4">
+        <button className="cursor-pointer">←</button>
+        <p className="text-center text-1xl font-bold">2024年10月</p>
+        <button className="cursor-pointer">→</button>
+      </div>
+
+      {/* フォーム */}
+      <form className="border px-4 py-4">
+        <dl className="">
+          <div className="">
+            <dt className="font-bold">記録日</dt>
+            <dd>
+              <input
+                type="date"
+                className="border rounded px-2 py-1 text-sm mt-2"
+              />
+            </dd>
+          </div>
+          <div className="mt-4">
+            <dt className="font-bold">就寝時間</dt>
+            <dd>
+              <input
+                type="time"
+                className="border rounded px-2 py-1 text-sm mt-2"
+              />
+            </dd>
+          </div>
+          <div className="mt-4">
+            <dt className="font-bold">起床時間</dt>
+            <dd>
+              <input
+                type="time"
+                className="border rounded px-2 py-1 text-sm mt-2"
+              />
+            </dd>
+          </div>
+        </dl>
+        <div className="mt-4">
+          <button className="w-full bg-gray-500 text-white rounded py-4 font-bold cursor-pointer">
+            記録
+          </button>
+        </div>
+      </form>
+      {/* フォームエンド */}
+
+      {/* タイムライン */}
+      <div className="py-4 px-4">
+        <h2 className="font-bold text-lg pt-2">今月の記録</h2>
+        <ul className="mt-4 max-h-[500px] overflow-auto">
+          <li className="grid grid-cols-6 items-center gap-2 py-4 border-b">
+            <p className="col-span-2">10/15</p>
+            <p>23:30</p>
+            <span className="">→</span>
+            <p>7:00</p>
+            <p>7h30m</p>
+          </li>
+          <li className="grid grid-cols-6 items-center gap-2 py-4 border-b">
+            <p className="col-span-2">10/16</p>
+            <p>23:20</p>
+            <span className="">→</span>
+            <p>6:50</p>
+            <p>7h30m</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
